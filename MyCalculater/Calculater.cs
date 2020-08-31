9fe0af8a3c137d360026971e2026febe0c6c0c02
Philip Lind Jensen <jogjensen@hotmail.com>
2020-08-31 09:19:48 +0200

    Add project files.

---
+++ MyCalculater/Calculater.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyCalculater
+{
+    public class Calculater
+    {
+
+        #region Doubles
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        #endregion
+
+        #region Ints
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        #endregion
+    }
+}
---
+++ MyCalculaterTests/CalculaterTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculater;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace MyCalculater.Tests
+{
+    [TestClass()]
+    public class CalculaterTests
+    {
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.AreEqual(9,Calculater.Multiply(3,3));
+            Assert.AreEqual(3.30,Calculater.Multiply(1.10,3));
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(10,Calculater.Add(5,5));
+            Assert.AreEqual(15,Calculater.Add(7.5,7.5));
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            Assert.AreEqual(3,Calculater.Divide(9,3));
+            Assert.AreEqual(10,Calculater.Divide(100,10));
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(10,Calculater.Subtract(15,5));
+            Assert.AreEqual(7.5,Calculater.Subtract(15,7.5));
+        }
+    }
+}
